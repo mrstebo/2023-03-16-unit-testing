69d7226cc7a86a6cc306781051472ead35c7b6ad
Steven Atkinson <steven.atkinson@clock.co.uk>
2023-02-24 22:47:12 +0000

    Made it so that they do not need to have all the characteristics

--- 02-sorting-hat/03-with-injected-checks/eligibility-checks/characteristic-check.ts
+++ 02-sorting-hat/03-with-injected-checks/eligibility-checks/characteristic-check.ts
@@ -4,7 +4,7 @@ import { EligibilityCheck } from "./eligibility-check";
 
 export class CharacteristicsCheck implements EligibilityCheck {
   isEligible(house: House, student: Student): boolean {
-    return house.desiredCharacteristics.every((characteristic) =>
+    return house.desiredCharacteristics.some((characteristic) =>
       student.characteristics.includes(characteristic)
     );
   }
