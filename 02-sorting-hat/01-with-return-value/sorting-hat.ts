0718f4399c02f64e0d4d3420fb52840e7447180f
Steven Atkinson <steven.atkinson@clock.co.uk>
2023-02-24 22:03:59 +0000

    Added example without events

---
+++ 02-sorting-hat/01-with-return-value/sorting-hat.ts
@@ -0,0 +1,42 @@
+import { House } from "../shared/house";
+import { Student } from "../shared/student";
+
+export class SortingHat {
+  /**
+   * Trigger the sorting of a student by placing the hat on their head.
+   *
+   * @param student The student to sort
+   * @returns The house the student has been allocated
+   * @throws {Error} If the student is ineligible for any house
+   */
+  placeOnHeadOf(student: Student): House {
+    return this.pickHouse(student);
+  }
+
+  /**
+   * Attempts to pick a house for the student based on their characteristics
+   *
+   * @param student The student to pick a house for
+   * @returns The house the student has been allocated
+   * @throws {Error} If the student is ineligible for any house
+   */
+  private pickHouse(student: Student): House {
+    if (student.characteristics.includes("bravery")) {
+      return House.Gryffindor;
+    }
+
+    if (student.characteristics.includes("hard work")) {
+      return House.Hufflepuff;
+    }
+
+    if (student.characteristics.includes("intelligence")) {
+      return House.Ravenclaw;
+    }
+
+    if (student.characteristics.includes("ambition")) {
+      return House.Slytherin;
+    }
+
+    throw new Error("Student is ineligible for any house");
+  }
+}
